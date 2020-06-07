3170018f1d0ae656c870031d93185a0e79100e64
AbnerHsu <ashianghsu@gmail.com>
2020-06-07 21:42:42 +0800

    add task component

---
+++ src/component/Task/Task.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Task = ({ taskName }) => {
+    return (
+        <div>
+            <h2>{taskName}</h2>
+            <p></p>
+        </div>
+    );
+};
+
+export default Task;
---
+++ src/tasks.js
@@ -0,0 +1,14 @@
+export const tasks = [
+    {
+        name: 'write tasks management app'
+    },
+    {
+        name: 'watch funny vedio'
+    },
+    {
+        name: 'study react'
+    }, 
+    {
+        name: 'pay bill'
+    }
+];
